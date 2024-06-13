0b5ea803d886f4dadfaf6b9d779c2aeeea7c69c7
kajalprasad824 <kajalprasad824@gmail.com>
2024-06-13 17:41:31 +0530

    "Transfer functions override"

--- contracts/MajorValidator.sol
+++ contracts/MajorValidator.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
@@ -30,8 +29,8 @@ contract MajorValidator is
     // NFT Id => NFTInfo
     mapping(uint256 => NFTInfo) public nftInfo;
 
-    event NFTMinted(uint _tokenId,string _finalId);
-    event NFTOwnerUpdated(uint _tokenId,address _currentOwner);
+    event NFTMinted(uint256 _tokenId, string _finalId);
+    event NFTOwnerUpdated(uint256 _tokenId, address _currentOwner);
 
     function initialize(address initialOwner) public initializer {
         __ERC721_init("MajorValidator", "MajorV");
@@ -49,15 +48,56 @@ contract MajorValidator is
         string memory finalId = string(abi.encodePacked(hash, id));
         nftInfo[TokenId] = NFTInfo(finalId, address(this));
 
-        emit NFTMinted(TokenId,finalId);
+        emit NFTMinted(TokenId, finalId);
     }
 
     function updateOwner(uint256 _tokenid, address _toeknidOwner)
         public
-        onlyOwner
     {
+        require(msg.sender == owner() || msg.sender == nftInfo[_tokenid].currentOwner);
         nftInfo[_tokenid].currentOwner = _toeknidOwner;
-        emit NFTOwnerUpdated(_tokenid,_toeknidOwner);
+        emit NFTOwnerUpdated(_tokenid, _toeknidOwner);
+    }
+
+    //------------Overriding all transfere function so that only------------------------
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override(ERC721Upgradeable, IERC721) onlyOwner {
+
+        if(ownerOf(tokenId) == address(this)){
+            _transfer(address(this), to, tokenId);
+        }
+        else{
+            _transfer(from, to, tokenId);
+        }
+    
+    }
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    // function safeTransferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 tokenId
+    // ) public virtual override(ERC721Upgradeable, IERC721) onlyOwner {
+    //     safeTransferFrom(from, to, tokenId, "");
+    // }
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override(ERC721Upgradeable, IERC721) onlyOwner {
+        
+        super.safeTransferFrom(from, to, tokenId, data);
     }
 
     // The following functions are overrides required by Solidity.
